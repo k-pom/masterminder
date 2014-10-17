8ae637e6736d619a6bf02914702cdad491aaa783
Kaleb Pomeroy <kaleb.pomeroy@rackspace.com>
2014-10-17 16:29:18 -0500

    Calm down a little on the pin checking

--- masterminder/inputs/pin.py
+++ masterminder/inputs/pin.py
@@ -1,19 +1,23 @@
+import time
 import RPi.GPIO as GPIO
 from masterminder.inputs import Input
 
 
 class PinInput(Input):
-    def __init__(self, number, name, pull_up_down=GPIO.PUD_DOWN):
+    def __init__(self, number, name, delay=1000, pull_up_down=GPIO.PUD_DOWN):
         """
             Inialize a new pin
             :param number: BCM or BOARD number
             :param name: The broadcasted event name
+            :param delay: Calm down on returning infinite True from check (ms)
             :param pull_up_down: GPIO.PUD_DOWN or GPIO.PUD_UP
         """
         self.number = number
         self.pull_up_down = pull_up_down
         self.name = name
         self.closed_state = self.get_closed_state()
+        self.last_closed_time = None
+        self.delay = delay
 
         GPIO.setup(self.number, GPIO.IN, self.pull_up_down)
 
@@ -22,8 +26,12 @@ class PinInput(Input):
             Check for the pin voltage and return if it is in an
             actionable state
         """
-        print "Checking %s for %s... is currently %s" % (self.number, self.closed_state, GPIO.input(self.number))
-        return (GPIO.input(self.number) == self.closed_state)
+        if GPIO.input(self.number) == self.closed_state:
+            current_time = now_in_ms()
+            if (current_time - self.last_check_time) > self.delay:
+                self.last_check_time = current_time
+                return True
+        return False
 
     def get_closed_state(self):
         """
@@ -33,3 +41,6 @@ class PinInput(Input):
             open (no voltage), represented as 0
         """
         return 1 if self.pull_up_down == GPIO.PUD_DOWN else 0
+
+def now_in_ms():
+    return int(round(time.time() * 1000))
