ba0d973e50e9831b0f978e5c03c210cf04c50f4e
Your Name <you@example.com>
2014-12-17 01:27:18 +0000

    Minor tweaks

--- masterminder/consumers/general.py
+++ masterminder/consumers/general.py
@@ -2,17 +2,11 @@ import os
 
 from masterminder.lib.broker import listen
 from masterminder.lib import broker
+from masterminder.consumers.pandora import current_station
 
 @listen("app.start")
 def app_start(data):
 
     os.system('su - pi -c "screen -dm -S pianobar pianobar"')
 
-    try:
-        with open(config['pandora_station'], 'r') as f:
-            station = f.read().replace('\n', '')
-    except:
-        station = 1
-        print "No pandora station found. Defaulting to 1"
-
-    broker.broadcast("pandora.station.set", station)
+    broker.broadcast("pandora.station.set", current_station())
--- masterminder/consumers/pandora.py
+++ masterminder/consumers/pandora.py
@@ -1,22 +1,24 @@
+import os
+
 from masterminder.lib.broker import listen
+from masterminder.lib import broker
 from masterminder.config import config
 
 @listen("gpio.station_down")
 def station_down(data):
-    print "Changing station down"
-    s = _station()
-    print "Current station %s" % s
-    set_station(s - 1, True)
+    s = current_station()
+    _ctl("s")
+    broker.broadcast("pandora.station.set", s - 1)
 
 @listen("gpio.station_up")
 def station_up(data):
-    print "Changing station up"
-    s = _station()
-    print "Current station %s" % s
-    set_station(s + 1, True)
+    s = current_station()
+    _ctl("s")
+    broker.broadcast("pandora.station.set", s + 1)
 
 @listen("gpio.pause")
 def pause(data):
+    print "Pausing..."
     _ctl("p")
 
 @listen("gpio.skip")
@@ -31,37 +33,28 @@ def thumbs_up(data):
 def thumbs_down(data):
     _ctl("-")
 
-@listen("gpio.change_station")
-def change_station(data):
-    """ Depending on the inputs, we may select a random station,
-        increase the station by one, or something else
-    """
-    _ctl("s")
-    set_station(1)  # Should this be a broadcast event? probably
-
-
 @listen("pandora.station.set")
-def set_station(data, use_s=False):
+def set_station(data):
     station = int(data)
     print "Writing %s  to file" % station
     with open(config['pandora_station'], 'w') as f:
         f.write(str(station))
     print "Changing station to %s" % station
 
-    if use_s:
-	_ctl("s%s\n" % station)
-    else:
-        _ctl("%s\n" % station)
+    _ctl("%s\n" % station)
 
 @listen("fifo.pandora.songstart")
 def songstart(data):
     pass
 
 def _ctl(letter):
+
     with open(config['pandora_ctl'], "w") as fp:
         fp.write(str(letter))
+    print "_ctl %s" % letter
+    print "-"*80
 
-def _station():
+def current_station():
     try:
         with open(config['pandora_station'], 'r') as f:
             return int(f.read().replace('\n', ''))
